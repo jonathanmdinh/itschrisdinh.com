82cfa6a44e89ba460c0e655ef8c8f2f7187ff66b
Jonathan Dinh <jonathanmdinh@gmail.com>
2023-03-03 22:05:35 -0500

    initial commit

---
+++ wp-content/themes/chrisdinh-photography/tailwind.config.cjs
@@ -0,0 +1,24 @@
+// https://tailwindcss.com/docs/configuration
+module.exports = {
+  content: [
+    './index.php',
+    './app/**/*.php',
+    './resources/**/*.{php,vue,js}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black: '#000',
+        white: '#fff',
+        grey: '#999999',
+      }, // Extend Tailwind's default colors,
+      screens: {
+        sm: '320px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1440px'
+      }
+    },
+  },
+  plugins: [],
+};

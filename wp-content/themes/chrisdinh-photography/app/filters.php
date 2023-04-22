31cd6f9dbcc139359fd70ce2803cc9d47a61930e
Daniel La <la.danieln@gmail.com>
2023-04-22 17:04:34 -0400

    Add ACF JSON load

--- wp-content/themes/chrisdinh-photography/app/filters.php
+++ wp-content/themes/chrisdinh-photography/app/filters.php
@@ -21,3 +21,15 @@ add_filter('excerpt_more', function () {
 add_filter('acf/settings/save_json', function() {
     return get_stylesheet_directory() . '/resources/acf-json';
 });
+
+add_filter('acf/settings/load_json', function my_acf_json_load_point( $paths ) {
+
+    // remove original path (optional)
+    unset($paths[0]);
+
+    // append path
+    $paths[] = get_stylesheet_directory() . '/resources/acf-json';
+
+    // return
+    return $paths;
+});

088bb3de6174ed8202388de90a43c0c64c3eda8e
Daniel La <la.danieln@gmail.com>
2024-08-14 20:24:39 -0400

    remove custom gallery for nano gallery

--- wp-content/themes/chrisdinh-photography/index.php
+++ wp-content/themes/chrisdinh-photography/index.php
@@ -5,6 +5,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://use.typekit.net/vxj8oei.css">
+
+    <!-- Nano gallery -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.1/jquery.min.js" type="text/javascript"></script>
+    <link  href="https://cdnjs.cloudflare.com/ajax/libs/nanogallery2/2.4.2/css/nanogallery2.min.css" rel="stylesheet" type="text/css">
+    <script  type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/nanogallery2/2.4.2/jquery.nanogallery2.min.js"></script>
     <?php wp_head(); ?>
 </head>
 
--- wp-content/themes/chrisdinh-photography/resources/views/page-gallery.blade.php
+++ wp-content/themes/chrisdinh-photography/resources/views/page-gallery.blade.php
@@ -12,53 +12,19 @@
     </header>
 
     @unless ( empty($galleryItems) )
-        <section class="gallery-items max-w-[1380px] mx-auto relative flex flex-row flex-wrap justify-center items-center gap-4 px-5 pb-20">
-            <div class="gallery-items__overlay absolute h-full w-full bg-black transition-all duration-700"></div>
-            @foreach ($galleryItems as $index => $item)
-                <div data-terms="{{ $item['taxonomy_terms'] }}" class="gallery-item__image inline-flex w-[150px] h-[150px] md:w-[250px] md:h-[250px] lg:w-[300px] lg:h-[300px] overflow-hidden cursor-pointer">
-                    <img
+        <section class="max-w-6xl mx-auto px-5 pb-20">
+            <div id="gallery" data-nanogallery2='{"thumbnailHeight": 300, "thumbnailWidth": "auto"}'>
+                @foreach ($galleryItems as $index => $item)
+                    <a
                         data-index="{{ $index }}"
-                        src="{{ $item['url'] }}"
-                        alt="{{ !empty($item['alt']) ? $item['alt'] : '' }}"
-                        class="relative object-cover block transition-all duration-1000 w-full h-full"
-                        data-height={{ $item['height'] }}
-                        data-width={{ $item['width'] }}
-                    >
-                </div>
-            @endforeach
+                        href="{{ $item['url'] }}"
+                        data-ngthumb="{{ $item['url'] }}">
+                        {{ !empty($item['alt']) ? $item['alt'] : '' }}
+                    </a>
+                @endforeach
+            </div>
         </section>
     @endunless
 
     @include('components.back-to-top')
-
-    <div class="gallery-popup fixed h-screen w-screen bg-black opacity-0 flex justify-center items-center top-0 left-0 right-0 bottom-0 -z-1 transition-all duration-300">
-        <button class="gallery-popup__close absolute top-10 right-[20px]" title="Close popup">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 stroke-white">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
-            </svg>
-        </button>
-        {{-- <div class="max-w-[90%] mx-auto flex flex-col md:flex-row flex-wrap justify-between items-center gap-y-5 md:gap-y-0">
-            <div class="order-2 w-full md:w-[100px] md:order-1">
-                <x-slider
-                    slider-settings-acf-name="gallery__thumbnail-slider-settings"
-                    slide-view-template-path="components.gallery-thumbnail-slide"
-                    :slide-view-template-data="$galleryItems"
-                    >
-                </x-slider>
-            </div>
-            <div class="order-1 w-full md:w-[calc(100%-200px)] md:order-2">
-                <x-slider
-                    slider-settings-acf-name="gallery__main-slider-settings"
-                    slide-view-template-path="components.gallery-main-slide"
-                    :slide-view-template-data="$galleryItems"
-                    >
-                </x-slider>
-            </div>
-        </div> --}}
-    </div>
-    <div class="gallery-popup__content">
-        <div class="gallery-popup__image-container relative flex justify-center items-center transition-all duration-300">
-            <img class="gallery-popup__image relative" />
-        </div>
-    </div>
 @endsection

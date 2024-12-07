fe5dcc6b10ff3e1ef9d5958f4df29938cf311323
Daniel La <daniel@Daniels-MacBook-Pro.local>
2024-12-07 17:02:06 -0500

    Remove gallery collections and restyle the page for nanoGallerys filters. Update styling of back to top button

--- wp-content/themes/chrisdinh-photography/resources/views/page-gallery.blade.php
+++ wp-content/themes/chrisdinh-photography/resources/views/page-gallery.blade.php
@@ -1,31 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-    <header class="gallery-header relative max-w-7xl mx-auto px-5 flex flex-col md:flex-row items-center">
-        <div class="gallery-header__heading basis-1/3 py-5">
+    <header class="gallery-header relative max-w-7xl mx-auto px-5 flex flex-col items-start">
+        <div class="gallery-header__heading basis-full py-5">
             <h1 class="block text-left font-neueHaas font-normal text-6xl tracking-[7px]">Gallery</h1>
         </div>
 
-        <div class="gallery-header__filters basis-2/3">
-            <x-gallery-collections class=""></x-gallery-collections>
+        <div class="gallery-header__filters basis-full">
+            <div class="gallery-collections block relative mx-auto pb-5 lg:py-7">
+                <p class="gallery-collections__description mb-4 text-grey tracking-[2px] w-full">Use the filters below to sort the images. Click on an image to take a closer look.</p>
+            </div>
         </div>
     </header>
 
     @unless ( empty($galleryItems) )
         <section class="max-w-7xl mx-auto px-5 pb-[100px]">
-            <div id="gallery" data-nanogallery2='{"thumbnailHeight": 300, "thumbnailWidth": "auto", "galleryFilterTags": true,
-          "galleryFilterTagsMode": "multiple", "galleryDisplayTransitionDuration": 1000,
-          "thumbnailDisplayTransition": "slideRight",
-          "thumbnailDisplayTransitionDuration": 300,
-          "thumbnailDisplayInterval": 150,
-          "thumbnailDisplayOrder": "colFromRight"}'>
+            <div id="gallery" data-nanogallery2="{{ $nanoGallerySettings }}">
                 @foreach ($galleryItems as $index => $item)
                     <a
-                        data-ngtags="{{ $item['taxonomy_terms'] }}"
+                        data-ngtags="{{ !empty($item['taxonomy_terms']) ? $item['taxonomy_terms'] : '' }}"
                         data-index="{{ $index }}"
                         href="{{ $item['url'] }}"
-                        data-ngthumb="{{ $item['url'] }}">
-                        {{ !empty($item['alt']) ? $item['alt'] : '' }}
+                        data-ngthumb="{{ !empty($item['sizes']['large']) ? $item['sizes']['large'] : $item['url'] }}">
                     </a>
                 @endforeach
             </div>
